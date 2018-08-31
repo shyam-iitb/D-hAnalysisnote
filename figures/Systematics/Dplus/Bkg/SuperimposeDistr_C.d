fcc3eae1ceb49e49fa6d6ff92d0b6ee615846e09
Simple-Shyam <shyam055119@gmail.com>
2018-08-31 22:23:02 +0530

    Wrong Plots

--- figures/Systematics/Dplus/Bkg/SuperimposeDistr_C.d
+++
@@ -1,4 +0,0 @@
-
-# DO NOT DELETE
-./SuperimposeDistr_C.so: /home/shyam/alice/sw/ubuntu1404_x86-64/ROOT/v5-34-30-alice7-1/include/cintdictversion.h /home/shyam/alice/sw/ubuntu1404_x86-64/ROOT/v5-34-30-alice7-1/include/RVersion.h
-SuperimposeDistr_C__ROOTBUILDVERSION= 5.34/30
--- figures/Systematics/Dplus/Bkg/run.C
+++
@@ -1,8 +0,0 @@
-{
-gSystem->SetIncludePath("-I. -I$ALICE_ROOT/include -I$ALICE_PHYSICS/include -I$ALICE_ROOT/ANALYSIS/macros -I$ROOTSYS/include");
-//gROOT->LoadMacro("AliDhCorrelationExtraction.cxx++g");
-//gROOT->LoadMacro("AliDhCorrelationExtractionOff.cxx++g");
-gROOT->ProcessLine(".L SuperimposeDistr.C++");
-gROOT->ProcessLine("SuperimposeDistr()");
-}
-
